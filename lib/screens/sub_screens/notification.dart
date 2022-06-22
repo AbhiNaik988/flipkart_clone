81a5e4eaae5be5a8e1812b1ecb21a3e56b1ced14
AbhiNaik988 <abhinaik988@gmail.com>
2022-06-22 14:33:30 +0530

    completed notification screen

--- lib/screens/sub_screens/notification.dart
+++ lib/screens/sub_screens/notification.dart
@@ -6,15 +6,61 @@ class NotificationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         elevation: 0,
         title: const Text(
           "Notifications",
-          style: TextStyle(fontWeight: FontWeight.normal),
+          style: TextStyle(
+            fontWeight: FontWeight.normal,
+          ),
         ),
       ),
-      body: const Center(
-        child: Text("Notifications"),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              margin: const EdgeInsets.fromLTRB(40, 10, 0, 10),
+              width: 150,
+              height: 200,
+              child: Image.asset(
+                "assets/notification.png",
+                fit: BoxFit.fill,
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: const Text(
+                "No notification yet",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            const Text(
+              "Simply browse, create a wishlist",
+              style: TextStyle(
+                color: Colors.grey,
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(bottom: 10),
+              child: const Text(
+                "or make a purchase",
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+            ElevatedButton(
+              child: const Text("Continue Shopping"),
+              onPressed: () {},
+            ),
+          ],
+        ),
       ),
     );
   }
